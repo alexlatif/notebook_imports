a5cc7ebcc2336782aa66da5f9ea3aca72a0fc21f
alelat19 <alelat19@gmail.com>
2023-04-01 17:13:34 +0300

    x

--- notebook_imports/main.py
+++ notebook_imports/main.py
@@ -22,6 +22,7 @@ def init():
     install(tf_version) # TODO will have to change this
     install("tensorflow-cloud")
     install("torch")
+    install("matplotlib")
     install("grpcio-status==1.48.2")
     install("google-cloud-aiplatform")
     install("google-cloud-storage")
